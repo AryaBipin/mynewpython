cddc639bbd5cc7ab4825c8f9f45f5af633549845
AryaBipin <110154938+AryaBipin@users.noreply.github.com>
2022-08-09 10:05:19 +0400

    y

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ from werkzeug.utils import secure_filename
 from PIL import Image,ImageDraw
 from waitress import serve 
 
-api = Flask(__name__)
+app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads'
-api.secret_key = "secret key"
-api.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
  
 
-@api.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@api.route('/favicon.ico')
+@app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico')
  
 
-@api.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_image():
     file = request.files['image']
     print (request.files['image'])
@@ -43,4 +43,4 @@ def upload_image():
     return render_template('index.html')
 if __name__ == "__main__":
     #app.run(debug=True,port=5001)
-    serve(api, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
