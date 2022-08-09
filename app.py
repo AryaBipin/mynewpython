f2a456f975a037995d76d496019c86d7a394cab9
AryaBipin <110154938+AryaBipin@users.noreply.github.com>
2022-08-09 08:47:09 +0400

    yty

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
  
 
 @app.route('/upload', methods=['POST'])
