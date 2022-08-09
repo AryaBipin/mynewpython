b441321e9885aef47766a10b7808abdacaef592c
AryaBipin <110154938+AryaBipin@users.noreply.github.com>
2022-08-09 16:47:28 +0400

    deleted

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def upload_image():
     background.save(os.path.join(app.config['UPLOAD_FOLDER'], "profile.png"))
     return render_template('index.html')
 if __name__ == "__main__":
-    app.run(debug=True,port=5002)
+    app.run(debug=True,port=5000)
    
